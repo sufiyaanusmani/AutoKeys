579c0d43d9617facb564fdd194db80495876e3d6
Sufiyaan Usmani <usmanisufiyaan@gmail.com>
2022-08-31 14:27:48 +0500

    added main.asm

---
+++ main.asm
@@ -0,0 +1,18 @@
+TITLE Project Name
+; Authors: Sufiyaan Usmani (21K-3195), Yousuf Ahmed (21K-4594), Qasim Hasan(21K-3210)
+; Project Link: https://github.com/sufiyaanusmani/COAL-Project
+
+INCLUDE Irvine32.inc
+
+.data
+    ; 
+
+.code
+    main PROC
+
+		exit
+    main ENDP
+
+; additional procedures here
+
+END main
